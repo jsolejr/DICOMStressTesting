4fdf04ef75e892b68482a3581dd8747eb7ae72f7
jsolejr <jsolejr@gmail.com>
2024-03-05 09:44:08 -0600

    Add files via upload

---
+++ Config.bat
@@ -0,0 +1,14 @@
+REM --Put your Connection information here -- 
+
+SET AE=hosp1rad
+SET SCP=127.0.0.1
+SET PORT=104
+
+REM --Specify your image size to test with --
+REM --Valid values: KB128,KB256,KB512,MB01,MB04,MB10 --
+SET IMGSZ=KB1281
+
+REM --Specify the Number of concurrent connections to open--
+SET CCNTS=3
+
+
