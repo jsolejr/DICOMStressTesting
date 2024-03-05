5936b8cc7b92d3ba3157fc04afbc2b271c1c2f19
John Sole <jsolejr@gmail.com>
2024-03-05 12:16:01 -0600

    Created a new file called TestPrepScript.ps1

--- Config.bat
+++ Config.bat
@@ -1,14 +1,12 @@
 REM --Put your Connection information here -- 
 
-SET AE=hosp1rad
+SET AE=AE_TITLE
 SET SCP=127.0.0.1
 SET PORT=104
 
 REM --Specify your image size to test with --
-REM --Valid values: KB128,KB256,KB512,MB01,MB04,MB10 --
+REM --Valid values: KB128,KB256,KB512,MB01,MB04,MB10 (Currently do not have files for 4 and 10 MB) --
 SET IMGSZ=KB1281
 
 REM --Specify the Number of concurrent connections to open--
 SET CCNTS=15
-
-
